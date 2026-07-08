7e15c53813b4883d03aefa2bbe5fcdd5c98b2591
LipartelianiNiko <niko.liparteliani.dev@gmail.com>
2026-07-08 18:22:23 +0400

    feat: added table, item fetching and dislaying, no pagination

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ItemList } from './components/item-list/item-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ItemList },
+];
--- frontend/src/app/components/item-list/item-list.ts
+++ frontend/src/app/components/item-list/item-list.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../../../services/itemService';
+import { Item } from '../../../models/models';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-item-list',
   imports: [],
   templateUrl: './item-list.html',
   styleUrl: './item-list.scss',
+  standalone: true,
 })
-export class ItemList {}
+
+export class ItemList implements OnInit {
+
+  items: Item[] = [];
+
+  constructor(private itemService: ItemService,   private cd: ChangeDetectorRef) {
+    console.log("CONSTRUCTOR");
+
+  }
+
+  ngOnInit(): void {
+    console.log("get All times request sent");
+    this.itemService.getItems().subscribe((data) => {
+      console.log("DATA RECEIVED", data);
+
+      this.items = data;
+
+      console.log("ITEMS NOW", this.items);
+
+    this.cd.detectChanges();
+    });
+  }
+}
