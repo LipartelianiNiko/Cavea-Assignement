bb7955304e53ff232ed812c798ac5fbba97325ac
LipartelianiNiko <niko.liparteliani.dev@gmail.com>
2026-07-10 20:16:11 +0400

    deployement: changes database connection configurations to support both deployement and local runniing

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,15 +3,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+//to support both deploement variables, local variables .
 const sequelize = new Sequelize({
   dialect: "postgres",
 
-  host: process.env.PGHOST || "localhost",
-  port: parseInt(process.env.PGPORT || "5432"),
+  host: process.env.DB_HOST || process.env.PGHOST || "localhost",
 
-  database: process.env.PGDATABASE || "inventory_db",
-  username: process.env.PGUSER || "postgres",
-  password: process.env.PGPASSWORD || "",
+  port: parseInt(
+    process.env.DB_PORT || process.env.PGPORT || "5432"
+  ),
+
+  database: process.env.DB_NAME || process.env.PGDATABASE || "inventory_db",
+
+  username: process.env.DB_USER || process.env.PGUSER || "postgres",
+
+  password: process.env.DB_PASSWORD || process.env.PGPASSWORD || "",
 
   logging: false,
 
