50b24d28318a58bff70458f2cf40ec4c4b83ff4c
LipartelianiNiko <niko.liparteliani.dev@gmail.com>
2026-07-10 20:14:15 +0400

    deployement: changes database connection configurations

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -5,12 +5,24 @@ dotenv.config();
 
 const sequelize = new Sequelize({
   dialect: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  port: parseInt(process.env.DB_PORT || "5432"),
-  database: process.env.DB_NAME || "inventory_db",
-  username: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "",
+
+  host: process.env.PGHOST || "localhost",
+  port: parseInt(process.env.PGPORT || "5432"),
+
+  database: process.env.PGDATABASE || "inventory_db",
+  username: process.env.PGUSER || "postgres",
+  password: process.env.PGPASSWORD || "",
+
   logging: false,
+
+  dialectOptions: process.env.PGHOST
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
 });
 
 export default sequelize;
