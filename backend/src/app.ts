3aef9c15368838e5aaf77f525ae72be81189a9ea
LipartelianiNiko <niko.liparteliani.dev@gmail.com>
2026-07-10 20:27:54 +0400

    deployement: added frontend link to cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,9 @@ import cors from "cors";
 import router  from "./controllers/routes";
 const app = express();
 
-app.use(cors(
-  origin: ['https://your-frontend.vercel.app', 'http://localhost:4200']
-));
+app.use(cors({
+  origin: ['https://cavea-assignement.vercel.app', 'http://localhost:4200']
+}));
 app.use(express.json());
 
 app.use("/api",router);
